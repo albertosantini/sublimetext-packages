29a71d0c1b4842ec43234af6a68b59cd0528a801
icebox <albertosantini@gmail.com>
2013-10-06 08:57:35 +0200

    Comment temp snippet.

--- User/z.py
+++ User/z.py
@@ -1,15 +1,18 @@
-import sublime_plugin
+# import sublime_plugin
 
-class My(sublime_plugin.EventListener):
-    def on_post_window_command(self, window, command_name, args):
-            print(command_name, args)
+# class My(sublime_plugin.EventListener):
+#     def on_post_window_command(self, window, command_name, args):
+#         print(command_name, args)
 
-            # if command_name == 'build':
-            #     output_panel = window.get_output_panel('exec')
-            #     errs = output_panel.find_all_results()
-            #     # print('Errors: ' + str(len(errs)))
-            #     if len(errs) == 0:
-            #         output_panel.run_command('hide_panel', {'panel': 'output.exec'})
+#         # if command_name == 'build':
 
-    # def on_post_text_command(self, window, command_name, args):
-    #         print(command_name, args)
+#     def on_post_save(self, view):
+#         window = view.window()
+#         output_view = window.create_output_panel('exec')
+#         if output_view.window():
+#             errs = output_view.find_all_results()
+#             # output_view.substr(sublime.Region(0, output_view.size()))
+#             if len(errs) == 0:
+#                 window.run_command("hide_panel", {"panel": "output.exec", cancel": True})
+#             # print(output_panel.substr(sublime.Region(0, view.size())))
+#             # window.run_command("show_panel", {"panel": "output.exec"})
