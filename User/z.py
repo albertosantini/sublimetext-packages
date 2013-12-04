efdce6d136e2755cee4ab78203cb1465c7afffee
icebox <albertosantini@gmail.com>
2013-12-04 23:04:55 +0100

    Add cycle loop for output errors.

--- User/z.py
+++ User/z.py
@@ -5,12 +5,16 @@ import re
 import importlib
 defaultExec = importlib.import_module("Default.exec")
 
+output_error_messages = []
+
 class ExecCommand(defaultExec.ExecCommand):
     def on_finished(self, proc):
         super(ExecCommand, self).on_finished(proc)
 
+        output_view = self.output_view
+
         view = self.window.active_view()
-        errs = self.output_view.find_all_results()
+        errs = output_view.find_all_results()
 
         if (len(errs) == 0 and proc.exit_code() == 0):
             self.window.run_command("hide_panel", {"cancel": True})
@@ -18,6 +22,9 @@ class ExecCommand(defaultExec.ExecCommand):
 
         else:
             regions = []
+
+            global output_error_messages
+            output_error_messages = self.getErrorMessages(output_view)
 
             for err in errs:
                 region = self.getAdjustedRegion(err[1], err[2])
@@ -49,6 +56,19 @@ class ExecCommand(defaultExec.ExecCommand):
 
         return region
 
+    def getErrorMessages(self, view):
+        view_error_messages = []
+        file_regex = str(view.settings().get("result_file_regex"))
+        group_regex = 3
+        err_regions = view.find_all(file_regex)
+        for index, err_region in enumerate(err_regions):
+            buf = str(view.substr(err_region))
+            error = re.findall(file_regex, buf)[0]
+            message = error[group_regex]
+            view_error_messages.append(message)
+
+        return view_error_messages
+
 class SublimeOnSaveBuild(sublime_plugin.EventListener):
     def on_post_save(self, view):
         filename_filter = "\\.(js|json|jshintrc|sublime-[\\w]+)$"
@@ -59,20 +79,40 @@ class SublimeOnSaveBuild(sublime_plugin.EventListener):
 
 class GotoError(sublime_plugin.TextCommand):
     def run(self, edit, direction):
-        err_regions = self.view.get_regions("exec_errors")
-        if (len(err_regions) == 0):
+        global output_error_messages
+
+        if (len(output_error_messages) == 0):
             return
+
+        err_messages = output_error_messages
+        err_regions = self.view.get_regions("exec_errors")
+        err_len = len(err_regions)
+
+        if (err_len == 0):
+            return
+
         caret = self.view.sel()[0].begin()
 
         if (direction == "prev"):
-            err_regions = reversed(err_regions)
+            err_messages.reverse()
+            err_regions.reverse()
+
+        err_last = err_regions[err_len - 1]
 
         for index, err_region in enumerate(err_regions):
             err_region_end = err_region.end()
             if ((direction == "next" and (caret < err_region_end)) or
                 (direction == "prev" and (caret > err_region_end))):
                 self.setCaret(err_region_end)
+                sublime.status_message(err_messages[index])
                 break
+
+        caret_new = self.view.sel()[0].begin()
+        if ((direction == "next" and (caret_new > err_last.end())) or
+            (direction == "prev" and (caret_new < err_last.end())) or
+            caret_new == caret):
+            self.setCaret(err_regions[0].end())
+            sublime.status_message(err_messages[0])
 
     def setCaret(self, position):
         self.view.sel().clear()
@@ -81,10 +121,8 @@ class GotoError(sublime_plugin.TextCommand):
 
 class GotoNextError(GotoError):
     def run(self, edit):
-        self.view.window().run_command("next_result")
         super(GotoNextError, self).run(edit, "next")
 
 class GotoPrevError(GotoError):
     def run(self, edit):
-        self.view.window().run_command("prev_result")
         super(GotoPrevError, self).run(edit, "prev")
