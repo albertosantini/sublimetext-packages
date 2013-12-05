3223ea4c562bd37dfca14a72070654de0a71e6ba
albertosantini <albertosantini@gmail.com>
2013-12-05 16:12:10 +0100

    Fix message and region messages.

--- User/z.py
+++ User/z.py
@@ -5,31 +5,29 @@ import re
 import importlib
 defaultExec = importlib.import_module("Default.exec")
 
-output_errors = []
+output_errors = {}
 
 class ExecCommand(defaultExec.ExecCommand):
     def on_finished(self, proc):
         super(ExecCommand, self).on_finished(proc)
 
-        output_view = self.output_view
-
         view = self.window.active_view()
+        output_view = self.output_view
         errs = output_view.find_all_results()
+
+        key = sublime.active_window().active_view().file_name()
+        key.replace("\\","/")
 
         if (len(errs) == 0 and proc.exit_code() == 0):
             self.window.run_command("hide_panel", {"cancel": True})
             view.erase_regions("exec_errors")
+            del output_errors[key]
 
         else:
-            regions = []
-
             global output_errors
-            output_errors = self.getErrors(output_view)
+            output_errors[key] = self.getErrors(output_view)
 
-            for err in errs:
-                region = self.getAdjustedRegion(err[1], err[2])
-                regions.append(region)
-
+            regions = output_errors[key]["error_regions"]
             view.add_regions("exec_errors", regions, "keyword", "dot",
                     sublime.DRAW_EMPTY |
                     sublime.DRAW_NO_FILL |
@@ -58,19 +56,28 @@ class ExecCommand(defaultExec.ExecCommand):
 
     def getErrors(self, view):
         view_errors = {
-            "output_view": view,
-            "regions": [],
-            "messages": []
+            "view": view,
+            "errors": [],
+            "error_regions": [],
+            "error_messages": [],
+            "output_regions": []
         }
+
         file_regex = str(view.settings().get("result_file_regex"))
-        group_regex = 3
         err_regions = view.find_all(file_regex)
         for err_region in err_regions:
             buf = str(view.substr(err_region))
             error = re.findall(file_regex, buf)[0]
-            message = error[group_regex]
-            view_errors["messages"].append(message);
-            view_errors["regions"].append(err_region);
+            region = self.getAdjustedRegion(error[1], error[2])
+            message = error[3]
+            view_errors["errors"].append((region, message, err_region))
+
+        view_errors["errors"] = sorted(view_errors["errors"])
+
+        for i, error in enumerate(view_errors["errors"]):
+            view_errors["error_regions"].append(view_errors["errors"][i][0])
+            view_errors["error_messages"].append(view_errors["errors"][i][1])
+            view_errors["output_regions"].append(view_errors["errors"][i][2])
 
         return view_errors
 
@@ -89,33 +96,36 @@ class GotoError(sublime_plugin.TextCommand):
         if (len(output_errors) == 0):
             return
 
-        err_output_view = output_errors["output_view"]
-        err_output_messages = output_errors["messages"]
-        err_output_regions = output_errors["regions"]
-        err_regions = self.view.get_regions("exec_errors")
-        err_len = len(err_regions)
+        key = sublime.active_window().active_view().file_name()
+        key.replace("\\","/")
+
+        output_view = output_errors[key]["view"]
+        error_regions = output_errors[key]["error_regions"]
+        error_messages = output_errors[key]["error_messages"]
+        output_regions = output_errors[key]["output_regions"]
+        err_len = len(error_regions)
 
         if (err_len == 0):
             return
 
         if (direction == "prev"):
-            err_output_messages = [x for x in reversed(err_output_messages)]
-            err_output_regions = [x for x in reversed(err_output_regions)]
-            err_regions = [x for x in reversed(err_regions)]
+            error_messages = [x for x in reversed(error_messages)]
+            output_regions = [x for x in reversed(output_regions)]
+            error_regions = [x for x in reversed(error_regions)]
 
         caret = self.view.sel()[0].begin()
-        for i, err_region in enumerate(err_regions):
+        for i, err_region in enumerate(error_regions):
             err_region_end = err_region.end()
             if ((direction == "next" and (caret < err_region_end)) or
                 (direction == "prev" and (caret > err_region_end))):
-                self.highlightBuildError(err_output_view, err_output_regions[i])
-                sublime.status_message(err_output_messages[i])
+                self.highlightBuildError(output_view, output_regions[i])
+                sublime.status_message(error_messages[i])
                 self.setCaret(err_region_end)
                 break
         else:
-            self.highlightBuildError(err_output_view, err_output_regions[0])
-            sublime.status_message(err_output_messages[0])
-            self.setCaret(err_regions[0].end())
+            self.highlightBuildError(output_view, output_regions[0])
+            sublime.status_message(error_messages[0])
+            self.setCaret(error_regions[0].end())
 
     def setCaret(self, position):
         self.view.sel().clear()
@@ -129,7 +139,6 @@ class GotoError(sublime_plugin.TextCommand):
         sublime.active_window().run_command("hide_panel", {"cancel": True})
         sublime.active_window().run_command("show_panel",
             {"panel": "output.exec"})
-
 
 class GotoNextError(GotoError):
     def run(self, edit):
