ef233ba5c83b4cf48b435db50f6c544c4254f20f
Alberto Santini <albertosantini@gmail.com>
2016-09-22 10:18:31 +0200

    Close build output view if there is no errors.

--- User/z.py
+++ User/z.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 import re
+import Default
 
 # Credits to https://github.com/alexnj/SublimeOnSaveBuild
 class SublimeOnSaveBuild(sublime_plugin.EventListener):
@@ -18,3 +19,15 @@ class SublimeOnSaveBuild(sublime_plugin.EventListener):
             return
 
         view.window().run_command("build")
+
+class ExecCommand(Default.exec.ExecCommand):
+    def on_finished(self, proc):
+        super(ExecCommand, self).on_finished(proc)
+
+        exit_code = proc.exit_code()
+        errors_len = len(self.output_view.find_all_results())
+
+        if (exit_code != None and exit_code != 0) or errors_len > 0:
+            self.window.run_command("show_panel", {"panel": "output.exec"})
+        else:
+            self.window.run_command("hide_panel", {"panel": "output.exec"})
