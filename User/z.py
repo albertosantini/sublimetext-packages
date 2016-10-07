b8ee3f10246e4cd8b00f6689b99dbd16b5edd128
Alberto Santini <albertosantini@gmail.com>
2016-10-07 09:13:41 +0200

    Remove inexistent OnSaveBuild setting.

--- User/z.py
+++ User/z.py
@@ -3,17 +3,9 @@ import sublime_plugin
 import re
 import Default
 
-# Credits to https://github.com/alexnj/SublimeOnSaveBuild
 class SublimeOnSaveBuild(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        global_settings = sublime.load_settings(self.__class__.__name__+ ".sublime-settings")
-
-        should_build = view.settings().get("build_on_save", global_settings.get("build_on_save", True))
-
         filename_filter = "\\.(js)$"
-
-        if not should_build:
-            return
 
         if not re.search(filename_filter, view.file_name()):
             return
