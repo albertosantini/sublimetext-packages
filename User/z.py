3abd3fb47994bc6f5a9c38f7330d4197245c0828
Alberto Santini <albertosantini@gmail.com>
2016-11-30 10:42:51 +0100

    Fix pep8 errors.

--- User/z.py
+++ User/z.py
@@ -3,14 +3,17 @@ import sublime_plugin
 import re
 import Default
 
-class SublimeOnSaveBuild(sublime_plugin.EventListener):
-    def on_post_save(self, view):
-        filename_filter = "\\.(js)$"
 
-        if not re.search(filename_filter, view.file_name()):
+class SublimeOnSaveBuild(sublime_plugin.EventListener):
+    def __init__(self):
+        self.filename_filter = "\\.(js)$"
+
+    def on_post_save(self, view):
+        if not re.search(self.filename_filter, view.file_name()):
             return
 
         view.window().run_command("build")
+
 
 class ExecCommand(Default.exec.ExecCommand):
     def on_finished(self, proc):
@@ -19,7 +22,8 @@ class ExecCommand(Default.exec.ExecCommand):
         exit_code = proc.exit_code()
         errors_len = len(self.output_view.find_all_results())
 
-        if (exit_code != None and exit_code != 0) or errors_len > 0:
+        if (exit_code is not None and exit_code != 0) or errors_len > 0:
             self.window.run_command("show_panel", {"panel": "output.exec"})
         else:
             self.window.run_command("hide_panel", {"panel": "output.exec"})
+
